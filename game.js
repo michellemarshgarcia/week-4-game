c5068261d09874d15396b19dfb10a195875d07ab
michellemarshgarcia <32890392+michellemarshgarcia@users.noreply.github.com>
2018-01-25 23:54:50 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,15 @@
+
+<script type="text/javascript">
+
+
+var crystalArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+var crystalValue = crystalArray[Math.floor(Math.random() * crystalArray.length)];
+var userChoice = 
+var playerScore = 0;
+var randomArray = ["0", "1", "2",..."120"];
+var randomNumber = randomArray[Math.floor(Math.random() *  randomArray.length)];
+var win = 0;
+var loss = 0;
+
+
+I need to create functions to generate different crystalValue on click button events then add the values together to determine a playerScore before comparing to the randomNumber. Alert win or loss and reset the crystalValues. I'm stuck at the buttons and use jQuery
